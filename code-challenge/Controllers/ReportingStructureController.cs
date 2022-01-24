54d76fcc34ce360308442a959b9b81985eecae59
Kyle <kfm4772@rit.edu>
2022-01-24 15:15:50 -0500

    Add files via upload

---
+++ code-challenge/Controllers/ReportingStructureController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using challenge.Services;
+using challenge.Models;
+
+namespace challenge.Controllers
+{
+    [Route("api/reportingstructure")]
+    public class ReportingStructureController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly IReportingStructureService _reportingStructureService;
+
+        public ReportingStructureController(ILogger<ReportingStructureController> logger, IReportingStructureService reportingStructureService)
+        {
+            _logger = logger;
+            _reportingStructureService = reportingStructureService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateReportingStructure(string employeedId)
+        {
+            _logger.LogDebug($"Received employee create request for '{employeedId}'");
+
+            var reportingStructure = _reportingStructureService.CreateReportingStructure(employeedId);
+
+            return CreatedAtRoute("createReportingStructure", new { id = employeedId }, reportingStructure);
+        }
+    }
+}
