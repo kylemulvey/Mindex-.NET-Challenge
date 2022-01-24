ab9f07b345002582e8e3d35dc6789ef4d63bd5e4
Kyle <kfm4772@rit.edu>
2022-01-24 15:15:25 -0500

    Create CompensationController.cs

---
+++ code-challenge/Controllers/CompensationController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using challenge.Services;
+using challenge.Models;
+
+namespace challenge.Controllers
+{
+    [Route("api/compensation")]
+    public class CompensationController : Controller
+    {
+        private readonly ILogger _logger;
+        private readonly ICompensationService _compensationService;
+
+        public CompensationController(ILogger<CompensationController> logger, ICompensationService compensationService)
+        {
+            _logger = logger;
+            _compensationService = compensationService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCompensation([FromBody] Compensation compensation)
+        {
+            _logger.LogDebug($"Received compensation create request.");
+
+            _compensationService.CreateCompensation(compensation);
+
+            return CreatedAtRoute("getCompensationById", new { id = compensation.Employee.EmployeeId }, compensation);
+        }
+
+        [HttpGet("{id}", Name = "getCompensationById")]
+        public IActionResult GetCompensationById(String id)
+        {
+            _logger.LogDebug($"Received compensation get request for '{id}'");
+
+            var compensation = _compensationService.GetCompensationById(id);
+
+            if(compensation == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(compensation);
+        }
+    }
+}
