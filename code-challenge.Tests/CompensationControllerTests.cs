aa8ae1d9a6594ad5117db85a2b1cf411c67c8c77
Kyle <kfm4772@rit.edu>
2022-01-24 15:27:39 -0500

    Add files via upload

---
+++ code-challenge.Tests/CompensationControllerTests.cs
@@ -0,0 +1,97 @@
+﻿using challenge.Controllers;
+using challenge.Data;
+using challenge.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using code_challenge.Tests.Integration.Extensions;
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using code_challenge.Tests.Integration.Helpers;
+using System.Text;
+
+namespace code_challenge.Tests.Integration
+{
+   
+    [TestClass]
+    public class CompensationControllerTests
+    {
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<TestServerStartup>()
+                .UseEnvironment("Development"));
+
+            _httpClient = _testServer.CreateClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        [TestMethod]
+        public void CreateCompensation_Returns_Created()
+        {
+            // Arrange
+            var compensation = new Compensation()
+            {
+                Employee = new Employee()
+                {
+                    EmployeeId = "8a55c6d2-41ec-0b02-cf256eea3755",
+                    FirstName = "Freddie",
+                    LastName = "Mercury",
+                    Department = "Business",
+                    Position = "CEO"
+                },
+                Salary = 1000000,
+                EffectiveDate = "4/1/22"
+            };
+
+            var requestContent = new JsonSerialization().ToJson(compensation);
+
+            //Execute
+            var postRequestTask = _httpClient.PostAsync("api/compensation",
+                new StringContent(requestContent, Encoding.UTF8, "application/json"));
+            var response = postRequestTask.Result;
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+
+            var newCompensation = response.DeserializeContent<Compensation>();
+            Assert.IsNotNull(newCompensation.Employee);
+            Assert.AreEqual(compensation.Salary, newCompensation.Salary);
+            Assert.AreEqual(compensation.EffectiveDate, newCompensation.EffectiveDate);
+        }
+
+        [TestMethod]
+        public void GetCompensationById_Returns_Ok()
+        {
+            // Arrange
+            var employeeId = "14ge8a8d-775c-a24b-e8c6e2f4fbga";
+            var expectedSalary = 42000;
+            var expectedEffectiveDate = "2/1/22";
+
+            // Execute
+            var getRequestTask = _httpClient.GetAsync($"api/compensation/{employeeId}");
+            var response = getRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            var compensation = response.DeserializeContent<Compensation>();
+            Assert.AreEqual(expectedSalary, compensation.Salary);
+            Assert.AreEqual(expectedEffectiveDate, compensation.EffectiveDate);
+        }
+    }
+}
---
+++ code-challenge.Tests/ReportingStructureControllerTests.cs
@@ -0,0 +1,76 @@
+﻿using challenge.Controllers;
+using challenge.Data;
+using challenge.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using code_challenge.Tests.Integration.Extensions;
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using code_challenge.Tests.Integration.Helpers;
+using System.Text;
+
+namespace code_challenge.Tests.Integration
+{
+    [TestClass]
+    class ReportingStructureControllerTests
+    {
+
+        private static HttpClient _httpClient;
+        private static TestServer _testServer;
+
+        [ClassInitialize]
+        public static void InitializeClass(TestContext context)
+        {
+            _testServer = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<TestServerStartup>()
+                .UseEnvironment("Development"));
+
+            _httpClient = _testServer.CreateClient();
+        }
+
+        [ClassCleanup]
+        public static void CleanUpTest()
+        {
+            _httpClient.Dispose();
+            _testServer.Dispose();
+        }
+
+        [TestMethod]
+        public void CreateReportingStructure_Returns_Created()
+        {
+            // Arrange
+            // Get employee John Lennon
+            var employeeId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+            var getRequestTask = _httpClient.GetAsync($"api/employee/{employeeId}");
+            var getResponse = getRequestTask.Result;
+            var employee = getResponse.DeserializeContent<Employee>();
+
+            var reportingStructure = new ReportingStructure()
+            {
+                Employee = employee,
+                NumberOfReports = 4
+            };
+
+            var requestContent = new JsonSerialization().ToJson(reportingStructure);
+
+            // Execute
+            var postRequestTask = _httpClient.PostAsync("api/reportingstructure",
+               new StringContent(requestContent, Encoding.UTF8, "application/json"));
+            var postResponse = postRequestTask.Result;
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.Created, postResponse.StatusCode);
+
+            var newReportingStructure = postResponse.DeserializeContent<ReportingStructure>();
+            Assert.IsNotNull(newReportingStructure.Employee);
+            Assert.AreEqual(reportingStructure.Employee, newReportingStructure.Employee);
+            Assert.AreEqual(reportingStructure.NumberOfReports, newReportingStructure.NumberOfReports);
+        }
+    }
+}
